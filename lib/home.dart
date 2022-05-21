331cbe3bd40e3efa5a4e39924f10b73550a053ab
FlutLab.io <>
2022-05-21 22:12:01 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -73,6 +73,61 @@ class Home extends StatelessWidget {
                     ],
                   ),
                 ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [
+                        0.3,
+                        1
+                      ],
+                      colors: [
+                        Color(0xFF2494F5),
+                        Color(0xFF095A9D),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          "IMC",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Imc_Page()));
+                        },
+                      ),
+                      FlatButton(
+                        child: Text(
+                          "IMC",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Imc_Page()));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ],
             )));
   }
