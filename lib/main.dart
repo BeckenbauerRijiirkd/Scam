608b7e0148188f6e1927acfd4e28e4ca733acc25
FlutLab.io <>
2022-05-24 13:04:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
 
-import 'package:cripto_moedas/configs/app_settings.dart';
-import 'package:cripto_moedas/configs/hive_config.dart';
-import 'package:cripto_moedas/repositories/conta_repository.dart';
-import 'package:cripto_moedas/repositories/favoritas_repository.dart';
-import 'package:cripto_moedas/services/auth_service.dart';
+import 'package:scam.com/configs/app_settings.dart';
+import 'package:scam.com/configs/hive_config.dart';
+import 'package:scam.com/services/auth_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,13 +21,7 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AuthService()),
-        ChangeNotifierProvider(create: (context) => ContaRepository()),
         ChangeNotifierProvider(create: (context) => AppSettings()),
-        ChangeNotifierProvider(
-          create: (context) => FavoritasRepository(
-            auth: context.read<AuthService>(),
-          ),
-        ),
       ],
       child: MeuAplicativo(),
     ),
