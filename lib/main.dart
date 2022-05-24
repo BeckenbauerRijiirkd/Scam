8d6b604e189105134007a78c37f8712f208f61b4
FlutLab.io <>
2022-05-24 14:08:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MultiProvider(
