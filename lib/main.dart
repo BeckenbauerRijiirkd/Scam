44652c4fff4ebf7281bb2fc22007952da024a851
FlutLab.io <>
2022-05-24 12:40:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,19 @@
-import 'package:flutter/material.dart';
-
-import 'login_page.dart';
-
+import 'services/auth_service.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'meu_aplicativo.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await MyApp();
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Login(),
-    );
-  }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: MeuAplicativo(),
+    ),
+  );
 }
