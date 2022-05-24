f24841eb033969756de53976d352c85053eb3f3a
FlutLab.io <>
2022-05-24 15:41:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,13 @@ import 'package:provider/provider.dart';
 import 'meu_aplicativo.dart';
 
 import 'package:scam.com/configs/app_settings.dart';
-import 'package:scam.com/configs/hive_config.dart';
 import 'package:scam.com/services/auth_service.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'meu_aplicativo.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
