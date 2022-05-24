4e2e366b978802771d476df0e35ba6d2fc52cda2
FlutLab.io <>
2022-05-24 15:44:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
 
   runApp(
     MultiProvider(
